c996b9a09f6896cf422146fc56ea2101f6bb4e2f
Johanna0630666 <umineko_2023@qq.com>
2025-04-03 02:00:58 +0800

    Update variebles.py

--- Practical4/variebles.py
+++ Practical4/variebles.py
@@ -1,26 +1,24 @@
-c=0
-a=15
-b=60+15
-c=a+b
-d=60+30
-e=5
-f=d+e
-if c>=f :                               #compare c and f
-    print("f is quicker than c")
-else:
-    print("c is quicker than f")        #That's the answer.
+a=15 # The walk to the bus stop is 15 mins. 
+b=75 # The bus journey takes 1 hr and 15 mins.
+c=a+b # The total time taken by bus is 90 mins.
+d=90 # The drive takes 1 hr and 30 mins.
+e=5 # The walk from the car park takes 5 mins.
+f=d+e # The total time taken by car is 95 mins.
+if c < f: # Compare c to e, and describe which method of commuting is quicker.
+    print("c<f, The bus transport method is quicker.") # The bus is quicker.
+elif c == f: # Compare c to e, and describe which method of commuting is quicker.
+    print("c=f, The bus and car transport methods take the same time.") # The bus and car take the same time.
+else: # Compare e to c, and describe which method of commuting is quicker.
+    print("f<c, The car transport method is quicker.") # The car is quicker.
 
-
-
+# Initialize boolean variables.
 X = True
 Y = False
-W = X and Y
-
-# Truth table for W:
-# X     Y     W
-# True  True  True
-# True  False False
-# False True  False
-# False False False
-
-print(W) #The table shows that W is True only when both X and Y are True. In all other cases, W is False.
+# Create W as the logical AND of X and Y.
+W = X and Y 
+#   X     | Y     | W
+#   True  | True  | True
+#   True  | False | False
+#   False | True  | False
+#   False | False | False 
+print(W) # This will print False.
