3ee92a503fd8da9bccffd034f86b3fe49bca73ab
Johanna0630666 <umineko_2023@qq.com>
2025-04-03 01:59:45 +0800

    Update mystery_code.py

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,21 +1,21 @@
 # What does this piece of code do?
-# Answer:Generate two random numbers from 1 to 6 until they are equal. If they are equal,terminate the loop; otherwise, continue.
+# Answer: This code is used to test how many times the computer runs before two random numbers are equal.
 
 # Import libraries
 # randint allows drawing a random number,
 # e.g. randint(1,5) draws a number between 1 and 5
+
 from random import randint
-
 # ceil takes the ceiling of a number, i.e. the next higher integer.
-# e.g. ceil(4.2)=5
-from math import ceil
+# e.g. ceil(4.2)=5 
 
-progress=0
-while progress>=0:
-	progress+=1                         #if not end, progress+1
-	first_n = randint(1,6)
-	second_n = randint(1,6)
-	if first_n == second_n:             #if equal, break, which means that the probability of the end is 1/6
-		print(progress)
-		break
+from math import ceil 
+progress=0 # Initialize progress to 0.
+while progress>=0: # Loop until progress is breaked.
+	progress+=1 # Increment progress by 1.                       
+	first_n = randint(1,6) # Draw a random number between 1 and 6.
+	second_n = randint(1,6) # Draw a random number between 1 and 6.
+	if first_n == second_n: # Check if the two numbers are equal.      
+		print(progress) # print progress (indicating how many times the computer has run)
+		break # Exit the loop if the two numbers are equal.
 
