1e7feb0b05b4b35d54768971fb81b77373102fd4
Johanna0630666 <umineko_2023@qq.com>
2025-05-28 09:50:40 +0800

    Update mystery_code.py

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: This code is used to test how many times the computer runs before two random numbers are equal.
+# Answer: This code tests how many iterations are needed before two randomly generated integers, each within the range 1 to 6 inclusive, are equal.
 
 # Import libraries
 # randint allows drawing a random number,
@@ -18,4 +18,3 @@ while progress>=0: # Loop until progress is breaked.
 	if first_n == second_n: # Check if the two numbers are equal.      
 		print(progress) # print progress (indicating how many times the computer has run)
 		break # Exit the loop if the two numbers are equal.
-
